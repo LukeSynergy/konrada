8e3747440a73d0f0ee20754884a93a7086d2297a
Lukasz Janicki <lj@intersynergy.pl>
2017-06-19 17:31:55 +0200

    formularze

--- src/AppBundle/Controller/FormularzController.php
+++ src/AppBundle/Controller/FormularzController.php
@@ -12,20 +12,48 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Translation\Translator;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use AppBundle\Form\FormularzType;
 
 class FormularzController extends Controller
 {
     /**
      * @Route("/show")
+     * 
+     * @Template
      */
     public function showAction()
     {
+        /* 
+        użyty template (pamiętaj o includowaniu use...) dlatego nie trzeba:
+        return $this->render('AppBundle:Formularz:show.html.twig', array(
+            'form' => $form->createView(),
+            'im' => $imie,
+        ));
+         */
+        $formularz = new Formularz();
+        $form = $this->createForm(\AppBundle\Form\FormularzType::class, $formularz);
+        return [
+            'form' => $form->createView(),
+        ];
+    }
+}
+/* 
+class FormularzController extends Controller
+{
+    /**
+     * @Route("/show")
+     
+    public function showAction()
+    {
         $formularzyk = new Formularz();
-        
-        $imie = $this->get('translator')->trans($name);
+         
+        $imie = $this->get('translator')->trans('formularz.name', [], 'formularz');
+        //zmienna w yml, potem tablica parametrow, ale jest pusta bo nie ma zadnej zmiennej przez % jak w togetee
+        //ostatni patametr - formularz, czyli nazwa pliku translacji w resources formularz.pl.yml
         
         $form = $this->createFormBuilder($formularzyk)
-        ->add('name', TextType::class, array('label_format' => $imie ))
+        ->add('name', TextType::class, array('label' => $imie))
         ->add('surname', TextType::class)
         ->add('email', EmailType::class)
         ->add('text', TextareaType::class)
@@ -33,7 +61,8 @@ class FormularzController extends Controller
         ->getForm();
         return $this->render('AppBundle:Formularz:show.html.twig', array(
             'form' => $form->createView(),
+            'im' => $imie,
         ));
     }
-
 }
+ */
--- src/AppBundle/Form/FormularzType.php
+++ src/AppBundle/Form/FormularzType.php
@@ -5,6 +5,9 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class FormularzType extends AbstractType
 {
@@ -13,7 +16,12 @@ class FormularzType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name')->add('surname')->add('email')->add('text');
+         $builder
+        ->add('name', TextType::class, array('label' => 'formularz.name'))
+        ->add('surname', TextType::class, array('label' => 'formularz.surname'))
+        ->add('email', TextType::class, array('label' => 'formularz.email'))
+        ->add('text', TextareaType::class, array('label' => 'formularz.text'))
+        ->add('send', SubmitType::class, array('label' => 'formularz.send'));
     }
     
     /**
@@ -22,7 +30,8 @@ class FormularzType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'AppBundle\Entity\Formularz'
+            'data_class' => 'AppBundle\Entity\Formularz',
+            'translation_domain' => 'formularz', //nazwa pliku bez pl/en.yml
         ));
     }
 
